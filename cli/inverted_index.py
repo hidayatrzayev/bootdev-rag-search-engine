5811fc252655488da4675f0f3fc62b318640f744
hidayatrzayev <rzayev.hidayat@gmail.com>
2026-08-17 00:40:09 +0200

    Implement term frequency

--- cli/inverted_index.py
+++ cli/inverted_index.py
@@ -1,5 +1,7 @@
 import pickle
 
+from collections import Counter
+
 from text_processor import TextProcessor
 from utils import load_movies
 
@@ -7,6 +9,8 @@ from utils import load_movies
 class InvertedIndex:
     __index: dict[str, set[int]] = {}
     __docmap: dict[int, dict] = {}
+    __term_frequencies: dict[int, Counter] = {}
+
     __text_processor: TextProcessor = TextProcessor()
 
     def get_documents(self, term) -> list[int]:
@@ -14,6 +18,13 @@ class InvertedIndex:
 
     def get_movie_by_doc_id(self, doc_id) -> dict:
         return self.__docmap[doc_id]
+
+    def get_tf(self, doc_id: int, term: str) -> int:
+        doc_counter = self.__term_frequencies.get(doc_id)
+        if doc_counter is None:
+            return 0
+
+        return doc_counter[term]
 
     def build(self):
         for movie in load_movies():
@@ -29,6 +40,9 @@ class InvertedIndex:
         with open("cache/docmap.pkl", "wb") as docmap_file:
             pickle.dump(self.__docmap, docmap_file)
 
+        with open("cache/term_frequencies.pkl", "wb") as tf_file:
+            pickle.dump(self.__term_frequencies, tf_file)
+
         return self
 
     def load(self):
@@ -38,6 +52,9 @@ class InvertedIndex:
         with open("cache/docmap.pkl", "rb") as docmap_file:
             self.__docmap = pickle.load(docmap_file)
 
+        with open("cache/term_frequencies.pkl", "rb") as tf_file:
+            self.__term_frequencies = pickle.load(tf_file)
+
     def __add_document(self, doc_id: int, text: str):
         stemmed_tokens = self.__text_processor.process_text(text)
         for token in stemmed_tokens:
@@ -45,3 +62,8 @@ class InvertedIndex:
                 self.__index[token].add(doc_id)
             else:
                 self.__index[token] = {doc_id}
+
+            if doc_id in self.__term_frequencies.keys():
+                self.__term_frequencies[doc_id].update({token})
+            else:
+                self.__term_frequencies[doc_id] = Counter({token})
--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -15,12 +15,14 @@ def parse_arguments(parser: ArgumentParser):
 
     subparsers.add_parser("build", help="Build an inverted index for the movies dataset")
 
+    tf_parser = subparsers.add_parser("tf", help="Get the term frequency for the given term in the document with the given ID")
+    tf_parser.add_argument("doc_id", type=int, help="ID of the document in which to look for the term frequency")
+    tf_parser.add_argument("term", type=str, help="Term whose frequency to get from the document")
+
     return parser.parse_args()
 
 
-def search_movie_by_query(query: str):
-    print(f"Searching for: {query}")
-
+def load_inverted_index() -> InvertedIndex:
     inverted_index = InvertedIndex()
     try:
         inverted_index.load()
@@ -28,6 +30,22 @@ def search_movie_by_query(query: str):
         print("No cached index file found - exiting program.")
         sys.exit(1)
 
+    return inverted_index
+
+
+def tokenize_single_term(term: str) -> str:
+    text_processor = TextProcessor()
+    token = text_processor.process_text(term)
+    if len(token) > 1:
+        raise ValueError(f"Expected to tokenize term {term} to exactly 1 token, but was {len(token)}")
+
+    return token[0]
+
+
+def search_movie_by_query(query: str):
+    print(f"Searching for: {query}")
+
+    inverted_index = load_inverted_index()
     text_processor = TextProcessor()
     tokens = text_processor.process_text(query)
 
@@ -51,6 +69,12 @@ def build_inverted_index():
     inverted_index.build().save()
 
 
+def get_tf_in_document(doc_id: int, term: str) -> int:
+    token = tokenize_single_term(term)
+    inverted_index = load_inverted_index()
+    return inverted_index.get_tf(doc_id, token)
+
+
 def main() -> None:
     parser = ArgumentParser(description="Keyword Search CLI")
     args = parse_arguments(parser)
@@ -60,6 +84,9 @@ def main() -> None:
             search_movie_by_query(args.query)
         case "build":
             build_inverted_index()
+        case "tf":
+            term_frequency = get_tf_in_document(args.doc_id, args.term)
+            print(f"Term frequency (TF) for term '{args.term}' in document with ID {args.doc_id} = {term_frequency}")
         case _:
             parser.print_help()
 
