6ac7113f6646ea6717d97b03374e8d33bb0572b3
hidayatrzayev <rzayev.hidayat@gmail.com>
2026-08-18 01:27:35 +0200

    Add and verify embeddings model

--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -1,7 +1,12 @@
 from argparse import ArgumentParser
 
+from cli.lib.semantic_search import verify_model
+
 
 def parse_arguments(parser: ArgumentParser):
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    subparsers.add_parser("verify", help="Verify embeddings model information")
     return parser.parse_args()
 
 
@@ -10,6 +15,8 @@ def main() -> None:
     args = parse_arguments(parser)
 
     match args.command:
+        case "verify":
+            verify_model()
         case _:
             parser.print_help()
 
