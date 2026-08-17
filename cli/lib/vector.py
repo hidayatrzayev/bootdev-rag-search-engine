ba3d928809e071bf3a51388c78458d18ad5e3d20
hidayatrzayev <rzayev.hidayat@gmail.com>
2026-08-18 01:47:25 +0200

    Implement cosine similarity of vectors

--- cli/lib/vector.py
+++ cli/lib/vector.py
@@ -22,3 +22,20 @@ def dot_product(vec1: Vector, vec2: Vector) -> float:
         raise ValueError(f"Vectors must have same lengths to perform a dot product: {vec1}; {vec2}")
 
      return sum([vec1[i] * vec2[i] for i in range(len(vec1))])
+
+
+def magnitude(vec: Vector) -> float:
+    return sum([vec[i] * vec[i] for i in range(len(vec))]) ** 0.5
+
+
+def cosine_similarity(vec1: Vector, vec2: Vector) -> float:
+   mag1 = magnitude(vec1)
+   if mag1 == 0:
+       return 0.0
+
+   mag2 = magnitude(vec2)
+   if mag2 == 0:
+       return 0.0
+   
+   return dot_product(vec1, vec2) / (mag1 * mag2)
+   
