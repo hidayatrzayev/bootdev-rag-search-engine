305b6995547bd1512094e56f0e65078d19b54670
hidayatrzayev <rzayev.hidayat@gmail.com>
2026-08-18 01:37:51 +0200

    Implement dot product of vectors

--- cli/lib/vector.py
+++ cli/lib/vector.py
@@ -12,6 +12,13 @@ def add_vectors(vec1: Vector, vec2: Vector) -> Vector:
 
 def subtract_vectors(vec1: Vector, vec2: Vector) -> Vector:
     if len(vec1) != len(vec2):
-            raise ValueError(f"Vectors must have same lengths to be subtracted: {vec1}; {vec2}")
+        raise ValueError(f"Vectors must have same lengths to be subtracted: {vec1}; {vec2}")
 
     return [vec1[i] - vec2[i] for i in range(len(vec1))]
+
+
+def dot_product(vec1: Vector, vec2: Vector) -> float:
+     if len(vec1) != len(vec2):
+        raise ValueError(f"Vectors must have same lengths to perform a dot product: {vec1}; {vec2}")
+
+     return sum([vec1[i] * vec2[i] for i in range(len(vec1))])
